8e4713bd2725e966c2ab1e006d18b32baa6b4516
kumudgarg <kumud50garg@gmail.com>
2021-03-29 14:15:46 +0530

    add config server

--- src/main/java/tyss/project/cloudconfigserver/CloudConfigServerApplication.java
+++ src/main/java/tyss/project/cloudconfigserver/CloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package tyss.project.cloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigServer
 @SpringBootApplication
 @EnableEurekaClient
 public class CloudConfigServerApplication {
